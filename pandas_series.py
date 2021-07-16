31e0e93ad3566c61832b741c78893944e206b326
Steve Newton <newton36@gmail.com>
2021-07-16 13:43:56 -0500

    series

--- pandas_series.py
+++ pandas_series.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 
 fruits = pd.Series(["kiwi", "mango", "strawberry", "pineapple",
 		"gala apple", "honeycrisp apple", "tomato", "watermelon",
@@ -16,15 +17,53 @@ Write the code to get only the string values containing the substring "berry".
 Write the code to get only the string values containing the substring "apple".
 Which string value contains the most vowels?
 """
-#are we really analyzing a random string of letters?
-#any conclusion will be pointless a priori
-#if it was an encoded message, that would be cool
+"""
+print(fruits.index)
+print(fruits.values)
+"""
+#shows unique (7-10)
+print(fruits.value_counts())
+#shows total
+print(fruits.describe())
+"""
+print(fruits.dtypes)
+print(fruits[-3:])
+"""
+#random fruits
+print([fruits[i] for i in np.random.choice(fruits.index, 2)])
+
+fruits.str.capitalize()
+fruits.str.count('a')
+
+#vowels
+vcount = lambda s: sum([s.count(v) for v in 'aeiou'])
+vs = vcount(fruits.str)
+
+max(fruits.str.len())
+fruits[fruits.str.len() > 4]
+
+#oos
+ohs = lambda s: s.count('o') > 1
+print(fruits[fruits.apply(ohs)])
+
+#berry/apple substring
+regex = re.compile('.*berry.*')
+lst = [s for s in fruits.values if re.match(regex, s)]
+print(lst)
+regex = re.compile('.*apple.*')
+lst = [s for s in fruits.values if re.match(regex, s)]
+print(lst)
+
+#voweliest
+print(fruits[np.argmax(vs)])
+print("\n")
+
+"""I'm not analyzing a random string of letters
 s = 'hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyf\
 	fsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwej\
 	ilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypu\
 	lzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyec\
 	uproguy'
-"""
 Which letter occurs the most frequently in the letters Series?
 Which letter occurs the Least frequently?
 How many vowels are in the Series?
@@ -32,24 +71,51 @@ How many consonants are in the Series?
 Create a Series that has all of the same letters but uppercased.
 Create a bar plot of the frequencies of the 6 most commonly occuring letters.
 """
-n = ['$796,459.41', '$278.60', '$482,571.67', '$4,503,915.98',
+
+n = pd.Series(['$796,459.41', '$278.60', '$482,571.67', '$4,503,915.98',
 	'$2,121,418.3', '$1,260,813.3', '$87,231.01', '$1,509,175.45',
 	'$4,138,548.00', '$2,848,913.80', '$594,715.39', '$4,789,988.17',
 	'$4,513,644.5', '$3,191,059.97', '$1,758,712.24', '$4,338,283.54',
-	'$4,738,303.38', '$2,791,759.67', '$769,681.94', '$452,650.23']
-
+	'$4,738,303.38', '$2,791,759.67', '$769,681.94', '$452,650.23'])
 """
 What is the data type of the numbers Series?
 How many elements are in the number Series?
 Perform the necessary manipulations by accessing Series attributes and methods to convert the numbers Series to a numeric data type.
 Run the code to discover the maximum value from the Series.
 Run the code to discover the minimum value from the Series.
-What is the range of the values in the Series?
+What is the range of the values in the Series? max-min
 Bin the data into 4 equally sized intervals or bins and output how many values fall into each bin.
 Plot the binned data in a meaningful way. Be sure to include a title and axis labels.
 """
+import matplotlib.pyplot as plt
+import termplotlib as tplt
 
-exam = [60, 86, 75, 62, 93, 71, 60, 83, 95, 78, 65, 72, 69, 81,
+clean = lambda x: float(x.strip('$').replace(',',''))
+n = n.apply(clean)
+print(n)
+
+#binning
+bins = 4
+bin_size = int(max(n)/bins)+1
+lst = []
+for i in range(1, bins+1):
+	#first exclude values already used
+	vals = n.values[(i-1)*bin_size <  n]
+	#then appends values under threshold
+	lst.append(vals[vals < i*bin_size])
+
+#elem per bin
+elem_count = [len(i) for i in lst]
+bin_edges = [0] + [i*bin_size for i in range(1, bins+1)]
+print("\nHistogram in terminal (bin range & count on left)")
+#histogram
+fig = tplt.figure()
+fig.hist(elem_count, bin_edges, orientation='horizontal')
+fig.show()
+
+##############################
+
+exams = [60, 86, 75, 62, 93, 71, 60, 83, 95, 78, 65, 72, 69, 81,
 		96, 80, 85, 92, 82, 78]
 
 """
@@ -60,17 +126,34 @@ Write the code necessary to implement a curve for your exam_grades Series and sa
 Use a method to convert each of the numeric values in the curved_grades Series into a categorical vale of letter grades. For example, 86 should be a 'B' and 95 should be an 'A'. Save this as a Series named letter_grades.
 Plot your new categorical letter_grades Series in a meaninful way and include a title and axis labels.
 """
+#drops ones and sums elements
+elems = pd.Series([10*(i//10) for i in exams]).value_counts()
+print(elems)
+bin_edges = sorted(list(elems.index) + [100])
+#sorting elements by bin index (60, 70, etc.)
+elem_count = [elems.values[i] for i in np.argsort(elems.index)]
+
+print("\nHistogram of exam scores (bin range & count on left)")
+fig = tplt.figure()
+fig.hist(elem_count, bin_edges, orientation='horizontal')
+fig.show()
+
+print("\n")
+###
+curved = np.array(exams) + 100 - max(exams)
+elems = pd.Series([10*(i//10) for i in curved]).value_counts()
+print(elems)
+bin_edges = sorted(list(elems.index) + [100])
+#sorting elements by bin index (60, 70, etc.)
+elem_count = [elems.values[i] for i in np.argsort(elems.index)]
+
+print("\nHistogram of curved exam scores (bin range & count on left)")
+fig = tplt.figure()
+fig.hist(elem_count, bin_edges, orientation='horizontal')
+fig.show()
 
 
 if __name__ == '__main__':
-	print(fruits.index)
-	print(fruits.values)
-	#shows unique (7-10)
-	print(fruits.value_counts())
-	#shows total
-	print(fruits.describe())
-	print(fruits.dtypes)
-	print(fruits[-3:])
-	print([fruits[i] for i in np.random.choice(fruits.index, 2)])
+	pass
 
 
